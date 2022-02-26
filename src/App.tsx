fdc3badcbf9b4a686ca3cde581fe968fa066a773
BadaHertz52 <lmlwpeace@naver.com>
2022-02-26 14:48:32 +0900

    Fix error by adding location and navigate in deps of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Loading from './components/Loading';
 import Navigation from './components/Navigation';
 import Home from './router/Home';
 import './assets/main.css';
-import { useEffect } from 'react';
 
 
 function App() {
   const location =useLocation();
   const navigate =useNavigate();
   useEffect(()=>{
-    location.pathname==="/" && navigate('/basic');
-  },[]);
+    location.pathname==="/" && navigate('/basic/');
+  },[location, navigate]);
+  
   return (
     <>
       <Navigation/>
       <div id="main">
         <Routes>
           <Route 
-            path="/basic"
+            path="/basic/"
             element={<Home/>}
           />
           <Route 
