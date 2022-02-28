4c16340dd036d0712b9aa30d0929849e48458fee
BadaHertz52 <lmlwpeace@naver.com>
2022-02-28 21:21:00 +0900

    FEAT) Make circle2

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
 const Loading =()=>{
+  let circle2_spans =[]
+  let i =0;
+  while (i<20 && circle2_spans.length<20){
+    circle2_spans.push(<span key={`circle2_span_${i}`} id={`circle2_span_${i}`}></span>);
+    i++;
+  };
+
   return (
     <div id="loading">
-      <section id="cricleLoading">
+      <section id="circleLoading">
         <div id="circle1" className='container'>
           <div className='loader'>
             <span></span>
           </div>
+        </div>
+        <div id="circle2" className='container'>
+          <div className='loader'>
+            {circle2_spans}
+          </div>
+          
         </div>
       </section>
     </div >
