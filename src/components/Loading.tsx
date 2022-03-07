d3655d48e3a63f7febd1e3a7277a90c2f1e2906b
BadaHertz52 <lmlwpeace@naver.com>
2022-03-07 22:25:05 +0900

    Make bar2 and Clean code of style about barLoading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Loading =()=>{
   let circle2_spans =[]
@@ -6,6 +6,39 @@ const Loading =()=>{
   while (i<20 && circle2_spans.length<20){
     circle2_spans.push(<span key={`circle2_span_${i}`} id={`circle2_span_${i}`}></span>);
     i++;
+  };
+
+  /*bar2*/
+  const bar2  = document.getElementById("bar2");
+  const bar2_bar   =document.getElementById("bar2_bar");
+  const bar2_percent  =document.getElementById("bar2_percent");
+  const innerWidth = window.innerWidth;
+  const [bar2Width, setBar2Width]=useState(0);
+  let percent =0;
+  const loadingBar2  =setInterval(showPercentBar2, 100);
+  function showPercentBar2 (){
+    if(bar2!==null 
+      && bar2_bar !==null 
+      &&bar2_percent !==null ){
+        if(percent ===100 ){
+          clearInterval(loadingBar2);
+          bar2_percent.textContent ="Complete!!";
+        }else {
+          percent =percent +1;
+          bar2_bar.style.width =  bar2Width *0.01 * percent + "px";
+          bar2_percent.textContent= percent +"%";
+      }
+  }};
+  
+  window.onresize = ()=>{
+    innerWidth> 700 ?
+    setBar2Width(innerWidth * 0.23):
+      innerWidth>320?
+      setBar2Width(innerWidth * 0.6):
+      setBar2Width (innerWidth *0.5)    
+    if(bar2 !==null){
+      bar2.style.width = bar2Width+ "PX";
+    }
   };
 
   return (
@@ -57,16 +90,20 @@ const Loading =()=>{
         <div>
           <div  id="bar1" className='container'>
             <div className='loader'>
-              <div className='bar'></div>  
+              <div id="bar1_bar" className='bar'>
+              </div>  
             </div>
             <div className='loading'>
               Loading...
             </div>
           </div>
+          <div id="bar2" className='container' >
+            <div className='loader' >
+              <div id="bar2_bar" className='bar'></div>
+            </div>
+            <div id="bar2_percent" className='loading'>%</div>
+          </div>
         </div>
-      </section>
-      <section id="percentLoading">
-
       </section>
     </div >
   )
