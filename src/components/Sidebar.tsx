e0ca0cee614cd1ef452433693af9f7fba1ae1853
BadaHertz52 <lmlwpeace@naver.com>
2022-03-12 18:43:42 +0900

    Modify width of sidebar according to innerWidth

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,7 +11,13 @@ const Sidebar =()=>{
   const onMenu =()=>{
     setOpen(!open);
     if(sidebar !==null ){
-      const width =window.innerWidth * 0.3;
+      const innerWidth =window.innerWidth
+      const width = innerWidth >=768 ? innerWidth *0.2 : 
+      innerWidth >=325? 
+      innerWidth *0.25 : 
+      innerWidth* 0.3;
+
+      
       if(open){;  
         sidebar.style.marginLeft = "0px"
       }else {
