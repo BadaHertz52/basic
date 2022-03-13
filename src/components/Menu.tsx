9084708e6859db6d5072542205c5129b69637527
BadaHertz52 <lmlwpeace@naver.com>
2022-03-13 17:15:55 +0900

    Refactory Menu to combine codes which are used repeatly as component and Use it

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,133 +1,123 @@
 import React from 'react';
 import {CgMenuRound} from 'react-icons/cg';
+import {BiCoffeeTogo} from 'react-icons/bi' ;
+import {AiOutlineSmile} from 'react-icons/ai';
+import coffe from '../assets/img/coffe.jpg';
+import beverage from '../assets/img/beverage.jpg';
+import bread from '../assets/img/bread.jpg';
 
 const Menu=()=>{
+  const onShowSide =()=>{
 
+  };
+  const SideButton =()=>{
+    return (
+      <button className='menuIcon' onClick={onShowSide}>
+        <CgMenuRound/>
+      </button>
+    )
+  }
+  const Side =()=>{
+    return(
+      <div className="side">
+        <div className='nav'>
+          <div className='name'>
+            Caffe &nbsp;
+              <AiOutlineSmile/>
+              <BiCoffeeTogo/>
+          </div>
+          <ul className='menubar'>
+            <li>Home</li>
+            <li>Product</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          <button className='logIn'>
+            <span>Log In</span>
+          </button>
+        </div>
+        <SideButton/>
+    </ div>
+    )
+  };
+  const Main =()=>{
+    return (
+      <div className="main">
+              <div className="main_section">
+                <img src={coffe} alt="coffe"/>
+                <div>
+                  Senectus et netus et malesuada.
+                </div>
+              </div>
+              <div className='main_section'>
+                <img src={beverage} alt="new beverage" />
+                <div> 
+                  id faucibus nisl tincidunt eget
+                </div>
+              </div>
+              <div className='main_section'>
+                <img src={bread} alt="bread"/>
+                <div>
+                Commodo odio aenean sed adipiscing diam donec
+                </div>
+              </div>
+            </div>
+    )
+  };
+  const Length =()=>{
+    return (
+      <div className="length">
+        <Side/>
+        <div className="header">
+          <div className='name'>
+            Caffe &nbsp;
+              <AiOutlineSmile/>
+              <BiCoffeeTogo/>
+          </div>
+          <div>
+            <button className='logIn'>
+              <span>Log In</span>
+            </button>
+            <SideButton/>
+          </div>
+        </div>
+        <Main/>
+      </div>
+    )
+  };
+
+  const Transverse =()=>{
+    return (
+      <div className="transverse">
+        <Side/>
+        <Main/>
+      </div>
+    )
+  }
   return(
     <div id="menus">
       <section id="menu1">
         <header>Type 1. Hide</header>
         <div className='menuBox'>
-          <div className='length'>
-            <div className='nav'>
-              <div className='name'>
-                Bada Cafe
-              </div>
-              <ul className='menubar'>
-                <li>Home</li>
-                <li>Product</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-              <button className='logIn'>
-                <span>Log In</span>
-              </button>
-            </div>
-            <div className='menuIcon'>
-              <CgMenuRound/>
-            </div>
-          </div>
-          <div className='length'>
-            <div className="nav">
-              <div className='name'>
-                Bada Cafe
-              </div>
-              <ul className='menubar'>
-                <li>Home</li>
-                <li>Product</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-              <button className='logIn'>
-                <span>Log In</span>
-              </button>  
-            </div>
-            <div className="menuIcon">
-              <CgMenuRound/>
-            </div>
-          </div>
+          <Length/>
+          <Length/>
         </div>
       </section>
       <section id="menu2">
-        <header>Type 2. Drop</header>
+        <header>Type 2. Drop </header>
         <div className='menuBox'>
-          <div className='transverse'>
-            <div className='name'>
-              Bada Cafe
-            </div>
-            <ul className='menubar'>
-              <li>
-                <span></span>
-                <span>Home</span>
-              </li>
-              <li>
-                <span></span>
-                <span>Product</span>
-              </li>
-              <li>
-                <span></span>
-                <span>About</span>
-              </li>
-              <li>
-                <span></span>
-                <span>Contact</span>
-              </li>
-            </ul>
-            <button className='logIn'>
-                <span>Log In</span>
-              </button>
-          </div>
-          <div className='transverse'>
-            <div className='name'>
-              Bada Cafe  
-            </div>
-            <ul className='menubar'>
-              <li>Home</li>
-              <li>Product</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-            <button className='logIn'>
-                <span>Log In</span>
-              </button>
-          </div>
+          <Transverse/>
+          <Transverse/>
         </div>
       </section>
       <section id="menu3">
-        <header>Type 3.</header>
+        <header>Type 3. </header>
         <div className='menuBox'>
-          <div className='length'>
-            <div className='name'>
-              Bada Cafe  
-            </div>
-            <ul className='menubar'>
-              <li>Home</li>
-              <li>Product</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-            <button className='logIn'>
-                <span>Log In</span>
-              </button>
-          </div>
-          <div className='transverse'>
-            <div className='name'>
-              Bada Cafe
-            </div>
-            <ul className='menubar'>
-              <li>
-                Home
-              </li>
-              <li>Product</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-            <button className='logIn'>
-                <span>Log In</span>
-            </button>
-          </div>
+          <Length/>
+          <Transverse/>
         </div>
       </section>
+
     </div>
   )
 };
