a0b3ee702543ac038c725d69e4de77e4a693f764
BadaHertz52 <lmlwpeace@naver.com>
2022-03-15 20:31:59 +0900

    Modify style of #menu1

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,17 +1,60 @@
-import React  from 'react';
+import React from 'react';
 import '../assets/practice.css'
 const Practice =()=>{
-  const array:number[] =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-
-  return (
-    <div id="practice">
-      <section>
-        <div className="loader">
-          {array.map(a=> <span></span>)}
-        </div>
-      </section>
-    </div>
-  )
+   const per =document.getElementById("per");
+   const bar = document.getElementById("bar");
+   let count =0;
+   const p =document.getElementById("p");
+   const onStart =()=>{
+      const loading =setInterval(percent, 80);
+      function percent(){
+         if(count ===100){
+            clearInterval(loading);
+            p.style.display ="none";
+            per.textContent ="COMPLETE"
+            per.style.fontWeight ="700";
+            per.style.fontSize= "1.2rem";
+            
+         }else{
+            count++;
+            bar.animate([
+            {},
+            {
+               strokeDashoffset:"0"
+            }
+            ],{
+               duration:100000,
+               easing:"linear",
+               fill:"both" 
+            });
+            per.textContent =String(count);
+            per.style.fontSize="2.5rem";
+            p.style.display ="block";
+         }
+      };
+   };
+   return (
+      <div id="practice">
+         <div className="box">
+            <div className="percent">
+               <svg>
+                  
+                  <circle cx="70" cy="70" r="70" />
+                  <circle cx="70" cy="70" r="70" id="bar"/>
+               </svg>
+               <div className="number">
+                  <div id="per"> 
+                     0
+                  </div>
+                  <span id="p">
+                     %
+                  </span>
+               </div>
+            </div>
+            <button className='btn' onClick={onStart}>start</button>   
+         </div>
+      </div>
+   )
 };
 
 
